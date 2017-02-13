46be84a5a8a76d6246bec7abdcb5d257c5abdd61
sebastian cruz <jsebas.ct@gmail.com>
2017-02-12 22:55:09 -0300

    chore: Configuration key mising

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
                 }
             }
         ]
-
+    },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
